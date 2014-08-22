3c730f815a4f105956f66158984e281f5725c57d
Mark Hall <mhall@pentaho.com>
2014-08-22 22:25:58 +1200

    [BACKLOG-153] Adding support for field types and counts.

--- data-profiling-api/src/main/java/com/pentaho/profiling/api/ProfilingField.java
+++ data-profiling-api/src/main/java/com/pentaho/profiling/api/ProfilingField.java
@@ -22,17 +22,107 @@
 
 package com.pentaho.profiling.api;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * Created by bryan on 7/31/14.
  */
 public class ProfilingField {
+
+  /** The name of the field */
   private String name;
 
+  /** The known types for this field (and potentially their counts) */
+  protected Map<String, ProfilingFieldType> types = new TreeMap<String, ProfilingFieldType>();
+
+  /**
+   * Get the name of this field
+   * 
+   * @return the name of this field
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * Set the name of this field
+   * 
+   * @param name
+   *          the name of this field
+   */
   public void setName( String name ) {
     this.name = name;
+  }
+
+  /**
+   * Get the ID of this field (hashcode of name)
+   * 
+   * @return the ID of this field
+   */
+  public int getID() {
+    return hashCode();
+  }
+
+  @Override
+  public int hashCode() {
+    int code = super.hashCode();
+    if ( name != null ) {
+      code = name.hashCode();
+    }
+
+    return code;
+  }
+
+  /**
+   * Add a new type or update the count of an existing type for this field
+   * 
+   * @param type
+   *          the type to add/update
+   */
+  public synchronized void addOrUpdateType( ProfilingFieldType type ) {
+    ProfilingFieldType existing = types.get( type.getTypeName() );
+    if ( existing == null ) {
+      existing = new ProfilingFieldType( type.getTypeName() );
+      types.put( existing.getTypeName(), existing );
+    }
+
+    existing.setCount( existing.getCount() + type.getCount() );
+  }
+
+  /**
+   * Remove the named type from the list of types for this field
+   * 
+   * @param typeName
+   *          the name of the type to remove
+   */
+  public synchronized void removeNamedType( String typeName ) {
+    types.remove( typeName );
+  }
+
+  /**
+   * Get an immutable iterator over the types for this field
+   * 
+   * @return
+   */
+  public Iterator<ProfilingFieldType> iterator() {
+    List<ProfilingFieldType> l = new ArrayList<ProfilingFieldType>( types.values() );
+
+    return Collections.unmodifiableList( l ).iterator();
+  }
+
+  /**
+   * Get the type info for the named type (or null if we have not seen examples of the named type for this field).
+   * 
+   * @param typeName
+   *          the name of the type to get info on
+   * @return
+   */
+  public ProfilingFieldType getNamedType( String typeName ) {
+    return types.get( typeName );
   }
 }
